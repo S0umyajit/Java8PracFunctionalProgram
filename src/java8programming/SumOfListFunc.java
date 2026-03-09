7f7408dbb5f7751a3ff19c4374642b48ed00abc5
S0umyajit <soumyajit2k18@gmail.com>
2026-03-09 13:55:48 +0530

    ListSumusing functionalProgramm

---
+++ src/java8programming/SumOfListFunc.java
@@ -0,0 +1,21 @@
+package java8programming;
+
+import java.util.List;
+
+public class SumOfListFunc {
+
+    public static void main(String[] args) {
+        List<Integer> nums= List.of(1,2,3,4,5,6);
+
+        int sum=addListFunc(nums);
+        System.out.println(sum);
+    }
+
+    private static int sum(int a, int b){
+        return a+b;
+    }
+    private static int addListFunc(List<Integer> nums) {
+        return nums.stream()
+                .reduce(0,SumOfListFunc::sum);
+    }
+}
